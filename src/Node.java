ccd826592af7b4284731ae1c218c1b78dbd3f66c
ochughtai <ochughtai@live.com>
2017-10-20 00:15:43 -0500

    Made some significant changes. Still trying to work through recursively comparing the trees. TODOs are marked

--- src/Node.java
+++ src/Node.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class Node<T>{
     private T data = null;
-    private List<Node> children = new ArrayList<>();
-    private Node parent = null;
+    private List<Node<T>> children = new ArrayList<Node<T>>();
+    private Node<T> parent = null;
 
     public Node(T data) {
         this.data = data;
     }
 
-    public void addChild(Node child) {
+    public void addChild(Node<T>child) {
         child.setParent(this);
         this.children.add(child);
     }
@@ -20,14 +20,14 @@ public class Node<T>{
         children.add(newChild);
     }
 
-    public void addChildren(List<Node> children) {
-        for(Node t : children) {
+    public void addChildren(List<Node<T>> children) {
+        for(Node<T> t : children) {
             t.setParent(this);
         }
         this.children.addAll(children);
     }
 
-    public List<Node> getChildren() {
+    public List<Node<T>> getChildren() {
         return children;
     }
 
@@ -39,11 +39,11 @@ public class Node<T>{
         this.data = data;
     }
 
-    private void setParent(Node parent) {
+    private void setParent(Node<T>parent) {
         this.parent = parent;
     }
 
-    public Node getParent() {
+    public Node<T>getParent() {
         return parent;
     }
 }
--- src/changeFinder.java
+++ src/changeFinder.java
@@ -1,22 +1,41 @@
-import java.io.File;
+import java.io.File; 
 import java.util.*;
 
 
 
 public class changeFinder{
 	
-	public static String getInput() {
+	//Allocate root nodes
+	static Node<File> root;
+	static Node<File> root2;
+	//Allocate Lists of Differences
+	static ArrayList<String> added = new ArrayList<String>();
+	static ArrayList<String> deleted = new ArrayList<String>();
+	
+	public static String getInput(int prompt) {
 		//Create scanner for System.in
 		Scanner scanner = new Scanner(System.in);
-		//Print Prompt
-		System.out.print("Directory for before and after comparison: ");
-		//Get input Address
-		String input = scanner.nextLine();
+		//Create string for user input
+		String input = null;
+		
+		if (prompt == 1){
+			
+			//Print Prompt
+			System.out.print("Directory for before and after comparison: ");
+			//Get input 
+			input = scanner.nextLine();
+		}
+		else if (prompt == 2){
+			//Print Prompt
+			System.out.print("Press Enter Once You Have Made Your Changes To Your Directory");
+			//Wait for user to hit enter
+			scanner.nextLine();
+		}
+		
 		//Close Scanner
 		scanner.close();
-		//Return
-		return input;
 		
+		return input;
 	}
 	
 	public static File openDir(String dir) {
@@ -33,14 +52,90 @@ public class changeFinder{
 		
 	}
 	
-	public static void getChildren(Node<String> parent, File start) {
-		//Get subdirectories/files
-		String[] children = start.list();
+	public static void getChildren(Node<File> parent) {
 		
-		for (String name : children) {
-			//TO-DO
-			File child = new File(name);
-			
+		//Get sub-directories/files
+		File[] files = parent.getData().listFiles();
+		
+		//Convert all sub-directories/files to nodes
+		List<Node<File>> children = new ArrayList<Node<File>>(); 
+		for (File file : files){
+			children.add(new Node<File>(file));
+		}
+		
+		//Add list of children to the parent node
+		parent.addChildren(children);
+		
+		//Recursively find and add all children
+		for (Node<File> dir : parent.getChildren()){
+			if (dir.getData().isDirectory()){
+				getChildren(dir);
+			}
+		}
+		
+		
+	}
+	
+	/*
+	 * Recursively compares the files from both trees and returns lists of differences 
+	 */
+	public static void compareFiles(Node<File> parent, Node<File> parent2){
+		
+		//Get children from both lists
+		List<Node<File>> children1 = parent.getChildren();
+		List<Node<File>> children2 = parent2.getChildren();
+		
+		//Allocate list of children in both trees
+		List<Node<File>> inBoth = null;
+		
+		//Find files added
+		for (Node<File> child2 : children2){
+			boolean found = false;
+			for (Node<File> child : children1){
+				if (child.getData().getAbsolutePath() == child2.getData().getAbsolutePath()) {
+					found = true;
+				}
+			}
+			if (!found){
+				added.add(child2.getData().getAbsolutePath());
+				//Recursively add it's children to the list
+				//TODO
+			}
+			else{
+				inBoth.add(child2);
+			}
+		}
+		
+		//Find files deleted
+		for (Node<File> child : children1){
+			boolean found = false;
+			for (Node<File> child2 : children2){
+				if (child.getData().getAbsolutePath() == child2.getData().getAbsolutePath()) {
+					found = true;
+				}
+			}
+			if (!found){
+				deleted.add(child.getData().getAbsolutePath());
+				//Recursively add its children to the list
+				//TODO
+			}
+			else{
+				inBoth.add(child);
+			}
+		}
+		//TODO
+		//Restructure this to check to see if the nodes are in both, otherwise do something
+		
+	}
+	
+	public static void printNodes(Node<File> root){
+		//Print every file in the list to the console
+		for (Node<File> file : root.getChildren()){
+			System.out.println(file.getData().getAbsolutePath());
+			//Recursively print children
+			if (file.getData().isDirectory()){
+				printNodes(file);
+			}
 		}
 		
 		
@@ -49,11 +144,20 @@ public class changeFinder{
 
 	public static void main(String[] args) {
 		//Get directory from user
-		String dir = getInput();
+		String dirPath = getInput(1);
 		//Open the directory
-		File start = openDir(dir);
-		//Create parent node for tree
-		Node<String> parent = new Node(start.getAbsolutePath());
+		File dir = openDir(dirPath);
+		//Create root node
+		root = new Node<File>(dir);
+		//Get the list of all the files in the directory
+		getChildren(root);
+		//Print all the files to console
+//		printFiles(root);
+		//Create root node for 2nd Tree
+		root2 = new Node<File>(dir);
+		//Get the list of all files in the directory
+		getChildren(root2);
+		
 
 	}
 
